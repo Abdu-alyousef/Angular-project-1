c9481518243cfb02ef4e0318fa7ab520d7559455
Abdu alyousef <boud.huseyin@gmail.com>
2021-11-14 17:12:09 +0100

    Formated the code

--- src/app/insta/auth/signup/signup.component.ts
+++ src/app/insta/auth/signup/signup.component.ts
@@ -8,29 +8,30 @@ import { AuthService } from '../auth.service';
   styleUrls: ['signup.component.css'],
 })
 export class SignupComponent {
-
   isLoading: boolean = false;
   error: string = '';
 
-    constructor(private authService: AuthService){}
-
+  constructor(private authService: AuthService) {}
 
   onSubmit(form: NgForm) {
-      if(!form.valid){
-        return;
-      }
-      const email = form.value.email;
-      const password = form.value.password;
-      this.isLoading = true
-        this.authService.signup(email, password).subscribe(resData => {
-          console.log(resData)
-          this.isLoading = false
-        }, errorMessage => {
-          console.log(errorMessage)
-          this.error = errorMessage
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    this.isLoading = true;
+    this.authService.signup(email, password).subscribe(
+      (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
 
-          this.isLoading = false
-        })
+        this.isLoading = false;
+      }
+    );
     form.reset();
   }
 }
